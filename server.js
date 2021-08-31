ecda2dd6b83dbb4b8a061034dcb7de86ac9375c2
Naveen Devinda <noobimplementsjs@gmail.com>
2021-08-31 22:32:52 +0530

    adding the app url as origin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https:rtcsignalling-63cad.web.app",
 		methods: ["GET", "POST"],
 		allowedHeaders: ["my-custom-header"],
 		credentials: true
@@ -15,7 +15,7 @@ const io = require("socket.io")(server, {
 });
 
 io.on('connection', (socket) => {
-	console.log('Client connected'+socket.id);
+	// console.log('Client connected'+socket.id);
 
 	socket.on('disconnect', () => {
 		console.log('Client disconnected');
@@ -28,58 +28,50 @@ io.on('connection', (socket) => {
 		canvas = getDataFromClient;
      });
 
-    
-    //  socket.to('room1').emit('I am room 1 from server');
-	// io.to("room 237").emit("a new user has joined the room"); 
-//https://stackoverflow.com/questions/17697199/socket-io-rooms-doesnt-work
-/*-----------------------------socketserve.io --------------------------------------------- */
+	socket.on("join_room",(room) => {
 
+		socket.join(room);//ok
+		// console.log("joined room is -"+room);
+		socket.broadcast.emit("join_room",room);//sent all clent
+		// console.log("All Rooms Array - "+room);
+	});
 
-//1-click join session and execute  socket.emi("join_room","123456")
-socket.on("join_room",(room) => {
+	socket.on('drawing',(msg)=> {
+		// var arr = ["room1","room2"],push();
+		let allRooms = socket.rooms;
+		let clientRooms = Array.from(allRooms)
+		//iterate through the rooms and emmit draw to all the rooms in the array
+		for(let i=0; i<=clientRooms.length; i++) {
+			socket.to(clientRooms[i]).emit("drawing",msg);
+		}
 
-    socket.join(room);//ok
-	console.log("joined room is -"+room);
-	socket.broadcast.emit("join_room",room);//sent all clent
-	// console.log("All Rooms Array - "+room);
-  });
+	});  
 
-  socket.on('drawing',(msg)=> {
-	 // var arr = ["room1","room2"],push();
-	let allRooms = socket.rooms;
-	let clientRooms = Array.from(allRooms)
-	//iterate through the rooms and emmit draw to all the rooms in the array
-	for(let i=0; i<=clientRooms.length; i++) {
-		socket.to(clientRooms[i]).emit("drawing",msg);
-	  }
+	socket.on('text', (msg)=> {
+		let allRooms = socket.rooms;
+		let clientRooms = Array.from(allRooms)
+		for(let i=0; i<=clientRooms.length; i++) {
+			socket.to(clientRooms[i]).emit("text",msg);
+		}
 
-  });  
+	})
 
-  socket.on('text', (msg)=> {
-	let allRooms = socket.rooms;
-	let clientRooms = Array.from(allRooms)
-	for(let i=0; i<=clientRooms.length; i++) {
-		socket.to(clientRooms[i]).emit("text",msg);
-	  }
+	socket.on('image', (msg)=>{
+		console.log(msg.src, msg.page, msg.title);
+		let allRooms = socket.rooms;
+		let clientRooms = Array.from(allRooms)
+		for(let i=0; i<=clientRooms.length; i++) {
+			socket.to(clientRooms[i]).emit("image",msg);
+		}
+	})
 
-})
-
-socket.on('image', (msg)=>{
-	console.log(msg.src, msg.page, msg.title);
-	let allRooms = socket.rooms;
-	let clientRooms = Array.from(allRooms)
-	for(let i=0; i<=clientRooms.length; i++) {
-		socket.to(clientRooms[i]).emit("image",msg);
-	  }
-})
-
-  socket.on("chat-message", (msg) => {
-	let allRooms = socket.rooms;
-	let clientRooms = Array.from(allRooms)
-	for(let i=0; i<=clientRooms.length; i++) {
-		socket.to(clientRooms[i]).emit("chat-message",msg);
-	  }
-  });
+	socket.on("chat-message", (msg) => {
+		let allRooms = socket.rooms;
+		let clientRooms = Array.from(allRooms)
+		for(let i=0; i<=clientRooms.length; i++) {
+			socket.to(clientRooms[i]).emit("chat-message",msg);
+		}
+	});
 
   });
 
