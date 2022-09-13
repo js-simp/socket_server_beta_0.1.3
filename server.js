fdbeeae50b725c40678d6c75e23afc5672b3812f
Naveen <nooobimplementsjs@gmail.com>
2022-09-13 23:08:22 +0530

    implementing array generation and handling play event

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const fs = require('fs');// for writing eventlog
+const readline = require('readline');
 const app = express();
 const server = http.createServer(app);
 
@@ -8,9 +9,45 @@ const PORT = process.env.PORT || 5000;
 
 
 
-// async function save_events(msg, timestamp) {
-// 	eventlog.push({msg, timestamp});
-// }
+async function get_events(sessionID, socket, callback) {
+	const fileStream = fs.createReadStream(`${sessionID}.txt`);
+  
+	const rl = readline.createInterface({
+	  input: fileStream,
+	  crlfDelay: Infinity
+	});
+	// Note: we use the crlfDelay option to recognize all instances of CR LF
+	// ('\r\n') in input.txt as a single line break.
+	let event_array = [];
+	let start;
+	  let end;
+	for await (const line of rl) {
+	  // Each line in input.txt will be successively available here as `line`.
+	  let event = JSON.parse(line);
+	  if("startTime" in event) {
+		start = event.startTime;
+		console.log(start);
+	  }
+	  else if ("endtime" in event) {
+		end = event.endtime;
+		console.log(end);
+	  }
+	  else {
+		event.time = event.time - start;
+		event_array.push(event)
+	  }
+	//   console.log(`Line from file: ${JSON.parse(line)}`);
+	}
+	  callback({
+		status: "ok",
+		duration : end - start
+	  });	
+
+	  socket.on("play", ()=> {console.log("Time to play!")});
+
+	// console.log(event_array);
+  }
+
 
 //socket server instance
   
@@ -33,11 +70,6 @@ io.on('connection', (socket) => {
 		console.log('Client disconnected');
 		// console.log(eventlog);
  
-	// writeFile function with filename, content and callback function
-	fs.writeFile('sessionId.json', JSON.stringify(eventlog), function (err) {
-  		if (err) throw err;
-  		console.log('File is created successfully.');
-		});
 		 con = "connected"});
 
 	socket.emit("me", socket.id);
@@ -100,8 +132,14 @@ io.on('connection', (socket) => {
 		
 	});
 
+	socket.on("regen", (sessionID, callback) => {
+		console.log(`We got to regenerate session ${sessionID}`);
+		get_events(sessionID, socket, callback);
+		
+	})
+
 	socket.on("endSession", () => {
-		fs.appendFile(`${my_room}.txt`, JSON.stringify({endtime : Date.now()}) + '\n', 
+		fs.appendFile(`${my_room}.txt`, JSON.stringify({endtime : Date.now()}) , 
 		function (err) {
 			if (err) throw err;
 			// console.log('Saved!');
